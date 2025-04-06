2ab3025fb7f4db5b9164c13908f1057bc82ddfc2
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-04-06 21:07:56 +0545

    removed creden

--- server/server/models.py
+++ server/server/models.py
@@ -4,7 +4,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
+#user model
 class UserModel(AbstractUser):
     userId=models.UUIDField(default=uuid.uuid4,unique=True,editable=False)
 
