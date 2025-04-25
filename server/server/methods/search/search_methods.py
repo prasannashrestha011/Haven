175552c9cd81f8e6067a5d82270d02c12383a410
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-04-25 20:26:46 +0545

    created two search methods

--- server/server/methods/search/search_methods.py
+++ server/server/methods/search/search_methods.py
@@ -1,7 +1,7 @@
 from re import search
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
-from server.models import RepositoryModel
+from server.models import RepositoryModel, UserModel
 
 
 class SearchRepositories:
@@ -13,6 +13,22 @@ class SearchRepositories:
             RepositoryModel.objects.annotate(
                 search=SearchVector("repoName", "des"),
                 rank=SearchRank(SearchVector("repoName", "des"), search_query),
+            )
+            .filter(search=search_query)
+            .order_by("-rank")
+        )
+        print("Search result ", queryset)
+        return queryset
+
+    @staticmethod
+    def find_users(username: str):
+        search_query = SearchQuery(username)
+        queryset = (
+            (
+                UserModel.objects.annotate(
+                    search=SearchVector("username"),
+                    rank=SearchRank(SearchVector("username"), search_query),
+                )
             )
             .filter(search=search_query)
             .order_by("-rank")
